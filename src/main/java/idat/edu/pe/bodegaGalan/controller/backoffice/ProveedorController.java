6c1db0609a0bebe684f4876513bbc566a5de024e
AJoseC <josecasanova.p1@gmail.com>
2023-12-06 18:40:54 -0500

    registro proveedores

--- src/main/java/idat/edu/pe/bodegaGalan/controller/backoffice/ProveedorController.java
+++ src/main/java/idat/edu/pe/bodegaGalan/controller/backoffice/ProveedorController.java
@@ -1,4 +1,39 @@
 package idat.edu.pe.bodegaGalan.controller.backoffice;
 
+import idat.edu.pe.bodegaGalan.model.bd.Producto;
+import idat.edu.pe.bodegaGalan.model.bd.Proveedor;
+import idat.edu.pe.bodegaGalan.model.request.ProductoRequest;
+import idat.edu.pe.bodegaGalan.model.request.ProveedoresRequest;
+import idat.edu.pe.bodegaGalan.model.response.ResultadoResponse;
+import idat.edu.pe.bodegaGalan.service.ProveedorService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Controller
+@RequestMapping("/backoffice/proveedor")
 public class ProveedorController {
+    private ProveedorService proveedorService;
+
+    @GetMapping("")
+    public String frmMantProveedor(Model model){
+        model.addAttribute("listaproveedores",
+                proveedorService.listarProveedores());
+        return "backoffice/product/frmproveedor";
+    }
+    @GetMapping("/listar")
+    @ResponseBody
+    public List<Proveedor> listarProveedores(){
+        return proveedorService.listarProveedores();
+    }
+
+    @PostMapping("/guardar")
+    @ResponseBody
+    public ResultadoResponse guardarProveedores(@RequestBody ProveedoresRequest proveedoresRequest){
+        return proveedorService.guardarProveedor(proveedoresRequest);
+    }
 }
--- src/main/java/idat/edu/pe/bodegaGalan/service/ProveedorService.java
+++ src/main/java/idat/edu/pe/bodegaGalan/service/ProveedorService.java
@@ -1,4 +1,34 @@
 package idat.edu.pe.bodegaGalan.service;
 
+
+import idat.edu.pe.bodegaGalan.model.bd.Proveedor;
+import idat.edu.pe.bodegaGalan.model.request.ProveedoresRequest;
+import idat.edu.pe.bodegaGalan.model.response.ResultadoResponse;
+import idat.edu.pe.bodegaGalan.repository.ProveedorRepository;
+
+import java.util.List;
+
 public class ProveedorService {
+    private ProveedorRepository proveedorRepository;
+
+    public List<Proveedor> listarProveedores(){ return proveedorRepository.findAll(); }
+
+    public ResultadoResponse guardarProveedor(ProveedoresRequest proveedoresRequest){
+        String mensaje = "Proveedor registrado correctamente";
+        Boolean respuesta = true;
+        try{
+            Proveedor proveedor = new Proveedor();
+            if(proveedoresRequest.getCod_proveedor() > 0){
+                proveedor.setCodigoProveedor(proveedoresRequest.getCod_proveedor());
+            }
+            proveedor.setNombre(proveedoresRequest.getNombre());
+            proveedor.setDireccion(proveedoresRequest.getDireccion());
+            proveedor.setTelefono(proveedoresRequest.getTelefono());
+            proveedorRepository.save(proveedor);
+        }catch (Exception ex){
+            mensaje = "Producto no registrado";
+            respuesta = false;
+        }
+        return ResultadoResponse.builder().mensaje(mensaje).respuesta(respuesta).build();
+    }
 }
