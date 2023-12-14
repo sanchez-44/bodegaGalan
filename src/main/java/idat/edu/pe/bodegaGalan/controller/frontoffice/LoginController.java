98863660ef57b582f5dedec6452e59631da21579
AJoseC <josecasanova.p1@gmail.com>
2023-12-13 20:40:53 -0500

    Avance 5

--- src/main/java/idat/edu/pe/bodegaGalan/controller/frontoffice/LoginController.java
+++ src/main/java/idat/edu/pe/bodegaGalan/controller/frontoffice/LoginController.java
@@ -9,10 +9,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @Controller
@@ -22,7 +20,10 @@ public class LoginController {
     private UsuarioService usuarioService;
 
     @GetMapping("/login")
-    public String login() {
+    public String login(@RequestParam(name = "error", required = false) String error, Model model) {
+        if (error != null) {
+            model.addAttribute("error", "Nombre de usuario o contraseña incorrectos");
+        }
         return "frontoffice/auth/frmLogin";
     }
 
